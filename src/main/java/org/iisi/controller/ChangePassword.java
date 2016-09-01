960fbb3aff4fcf0afe5f9ea30ef9935d8ea5ac10
user <user@192.168.201.32>
2016-09-01 16:03:04 +0800

    change password completed

---
+++ src/main/java/org/iisi/controller/ChangePassword.java
@@ -0,0 +1,58 @@
+package org.iisi.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.iisi.db.JDBCChangePwd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+@ManagedBean(name = "changepassword")
+@SessionScoped
+
+public class ChangePassword implements Serializable {
+	private static final long serialVersionUID = -2322823282417821899L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ChangePassword.class);
+	private String oldPwd;
+	private String newPwd;
+	private String checkPwd;
+	public String change(){
+		String eid;
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) context.getExternalContext().getSession(true);
+		eid = (String) session.getAttribute("eid");
+		LOGGER.debug(oldPwd);
+		LOGGER.debug(newPwd);
+		LOGGER.debug(checkPwd);
+
+
+		JDBCChangePwd db = new JDBCChangePwd();	
+		int status = db.ChangePwd(eid, oldPwd,newPwd);
+		LOGGER.debug("Change password status is "+status);
+
+		return "changepwd.xhtml";
+	}
+	public String getOldPwd() {
+		return oldPwd;
+	}
+	public void setOldPwd(String oldPwd) {
+		this.oldPwd = oldPwd;
+	}
+	public String getNewPwd() {
+		return newPwd;
+	}
+	public void setNewPwd(String newPwd) {
+		this.newPwd = newPwd;
+	}
+	public String getCheckPwd() {
+		return checkPwd;
+	}
+	public void setCheckPwd(String checkPwd) {
+		this.checkPwd = checkPwd;
+	}
+
+}
